82108d3f4e34eafa84e857849dc107680ad4aded
joel olausson <joel.olaus@gmail.com>
2020-04-01 14:11:04 +0200

    yeah

--- RecipeSearch/src/recipesearch/ListItem/ListItem.java
+++ RecipeSearch/src/recipesearch/ListItem/ListItem.java
@@ -1,4 +1,4 @@
-package recipesearch;
+package recipesearch.ListItem;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -6,19 +6,20 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
+import recipesearch.RecipeSearchController;
 import se.chalmers.ait.dat215.lab2.Recipe;
 
 import java.io.File;
 import java.io.IOException;
 
-public class RecipeListItem extends AnchorPane {
+public class ListItem extends AnchorPane {
 
     private RecipeSearchController parentController;
     private Recipe recipe;
     @FXML   ImageView ItemPicture;
     @FXML   Text ItemName;
 
-    public RecipeListItem(Recipe recipe, RecipeSearchController recipeSearchController){
+    public ListItem(Recipe recipe, RecipeSearchController recipeSearchController){
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ListItem.fxml"));
         fxmlLoader.setRoot(this);
         fxmlLoader.setController(this);
